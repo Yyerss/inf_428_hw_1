7176daaa9ccb304c3e6ea64c88573e511ac3f998
Syrkhan Madiyev <91780657+Syrhundred@users.noreply.github.com>
2024-11-26 12:23:56 +0500

    Update utils.py

--- task_3/utils.py
+++ task_3/utils.py
@@ -1,5 +1,7 @@
-def time_difference(hour1, hour2):
-    diff = abs(hour2 - hour1)
-    if diff > 12:
-        diff = 24 - diff
-    return diff
+import math
+
+def time_to_cyclic_features(hour):
+    if not (0 <= hour < 24):
+        raise ValueError("Hour must be в диапазоне [0, 24)")
+    radians = (hour / 24) * 2 * math.pi
+    return round(math.sin(radians), 4), round(math.cos(radians), 4)
